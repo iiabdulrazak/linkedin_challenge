a95450b61eaae1804239e3ab33fb70d651078e76
Abdulrazak A. Osman <zaak0101@hotmail.com>
2022-04-28 14:55:38 +0300

    new script: analysis & etl of jobFunction dataset

--- scripts/analysisProcess.py
+++ scripts/analysisProcess.py
@@ -9,23 +9,20 @@ client =  Minio('minio:9000', access_key="admin", secret_key="Zaak@1101", secure
 #Getting desired file
 dataMinio = client.get_object(
     "data-bucket",
-    "data/newFollowers.csv",
+    "data/jobFunction.csv",
 )
 
 #Converting minio object to pandas dataframe
 dataset = pd.read_csv(dataMinio, index_col='Unnamed: 0')
 
-#Converting pandas object to datetime
-dataset['Date'] = pd.to_datetime(dataset['Date'])
-
 #Lets see total days collected
-print(f'Total Days: {len(dataset)} Day')
+print(f'Total Job Functions: {len(dataset)}')
 
 #Replacing columns name with small letters
-dataset.columns = ['date','sponsored_followers','organic_followers','total_followers']
+dataset.columns = ['jobFunction','total_followers']
 
 #Connecting to our dockerized postgresql db
 engine = create_engine('postgresql://zaak:Zaak1234@postgres:5432/datadb')
 
 #pushing dataset to the database
-dataset.to_sql('newFollowers', engine)
+dataset.to_sql('jobFunction', engine)
